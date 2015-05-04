287bcb64fc93cd631b2c23c7a31de0a9c4ed4bed
maheshkkolla <mahesh.mk.245@gmail.com>
2015-05-04 15:43:12 +0530

    [Mahesh] Session and Cookies are created. Login is working Showing error message when invalid credentials of login Cookies expire should be done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'users/login' => 'users#login'
+  post 'users/validate'
   get 'users/new' => 'users#new' , :as => :user
-  get 'users/:id/profile' => 'users#profile'
+  get 'users/profile' => 'users#profile'
   get 'users/:id/edit' => 'users#edit'
   get 'events/show'
 
   resource :users
   resource :events
-  root 'users#index'
+  root 'users#login'
 
 end
